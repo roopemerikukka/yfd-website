c7ebc3ceded7f096ef790b2773be982382b2692a
Joonas Salovaara <joonas.salovaara@gmail.com>
2018-07-17 14:26:09 +0300

    [ref #25] (#26)
    - Load fonts with a simple link in head
    - Set base font-family to Muli
    - Load normalize.css from Cloudflare CDN with a simple link in head
    - Set global box-sizing to border box

--- pages/_app.js
+++ pages/_app.js
@@ -35,30 +35,50 @@ export default class MyApp extends App {
           <Component {...pageProps} />
         </PageTransition>
         <style jsx global>{`
+          /* Page transitions */
           .page-transition-enter {
             opacity: 0;
             transform: translate3d(0, 20px, 0);
           }
+          
           .page-transition-enter-active {
             opacity: 1;
             transform: translate3d(0, 0, 0);
             transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
           }
+
           .page-transition-exit {
             opacity: 1;
           }
+
           .page-transition-exit-active {
             opacity: 0;
             transition: opacity ${TIMEOUT}ms;
           }
+
           .loading-indicator-appear,
           .loading-indicator-enter {
             opacity: 0;
           }
+
           .loading-indicator-appear-active,
           .loading-indicator-enter-active {
             opacity: 1;
             transition: opacity ${TIMEOUT}ms;
+          }
+
+          /* Set global box sizing to border-box */
+          html {
+            box-sizing: border-box;
+          }
+
+          *, *:before, *:after {
+            box-sizing: inherit;
+          }
+
+          /* Base font styles */
+          html,body {
+            font-family: 'Muli', sans-serif;
           }
         `}</style>
       </Container>
--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,8 @@ export default class MyDocument extends Document {
             name='viewport'
             content='initial-scale=1.0, width=device-width'
           />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
+          <link href="https://fonts.googleapis.com/css?family=Muli:400,600,600i,700" rel="stylesheet" />
         </Head>
         <body>
           <Main />
