d0f2291a43d7ec85590b63a73bb0c2eed6a275e0
Daniel <drx.patty@gmail.com>
2018-07-17 18:18:35 +0300

    [ref #30]
    - Adds wrapper component for layout
    
    This closes #30.

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import App, { Container } from 'next/app'
 import { PageTransition } from 'next-page-transitions'
 
 import Loader from '../components/loader'
+import Wrapper from '../components/wrapper'
 
 const TIMEOUT = 400
 
@@ -10,8 +11,8 @@ export default class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {}
 
-    if ( Component.getInitialProps ) {
-      pageProps = await Component.getInitialProps( ctx )
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
     }
 
     return { pageProps }
@@ -21,66 +22,68 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props
     return (
       <Container>
-        <PageTransition
-          timeout={TIMEOUT}
-          classNames='page-transition'
-          loadingComponent={<Loader />}
-          loadingDelay={500}
-          loadingTimeout={{
-            enter: TIMEOUT,
-            exit: 0
-          }}
-          loadingClassNames='loading-indicator'
-        >
-          <Component {...pageProps} />
-        </PageTransition>
-        <style jsx global>{`
-          /* Page transitions */
-          .page-transition-enter {
-            opacity: 0;
-            transform: translate3d(0, 20px, 0);
-          }
-          
-          .page-transition-enter-active {
-            opacity: 1;
-            transform: translate3d(0, 0, 0);
-            transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
-          }
+        <Wrapper>
+          <PageTransition
+            timeout={TIMEOUT}
+            classNames='page-transition'
+            loadingComponent={<Loader />}
+            loadingDelay={500}
+            loadingTimeout={{
+              enter: TIMEOUT,
+              exit: 0
+            }}
+            loadingClassNames='loading-indicator'
+          >
+            <Component {...pageProps} />
+          </PageTransition>
+          <style jsx global>{`
+            /* Page transitions */
+            .page-transition-enter {
+              opacity: 0;
+              transform: translate3d(0, 20px, 0);
+            }
 
-          .page-transition-exit {
-            opacity: 1;
-          }
+            .page-transition-enter-active {
+              opacity: 1;
+              transform: translate3d(0, 0, 0);
+              transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+            }
 
-          .page-transition-exit-active {
-            opacity: 0;
-            transition: opacity ${TIMEOUT}ms;
-          }
+            .page-transition-exit {
+              opacity: 1;
+            }
 
-          .loading-indicator-appear,
-          .loading-indicator-enter {
-            opacity: 0;
-          }
+            .page-transition-exit-active {
+              opacity: 0;
+              transition: opacity ${TIMEOUT}ms;
+            }
 
-          .loading-indicator-appear-active,
-          .loading-indicator-enter-active {
-            opacity: 1;
-            transition: opacity ${TIMEOUT}ms;
-          }
+            .loading-indicator-appear,
+            .loading-indicator-enter {
+              opacity: 0;
+            }
 
-          /* Set global box sizing to border-box */
-          html {
-            box-sizing: border-box;
-          }
+            .loading-indicator-appear-active,
+            .loading-indicator-enter-active {
+              opacity: 1;
+              transition: opacity ${TIMEOUT}ms;
+            }
 
-          *, *:before, *:after {
-            box-sizing: inherit;
-          }
+            /* Set global box sizing to border-box */
+            html {
+              box-sizing: border-box;
+            }
 
-          /* Base font styles */
-          html,body {
-            font-family: 'Muli', sans-serif;
-          }
-        `}</style>
+            *, *:before, *:after {
+              box-sizing: inherit;
+            }
+
+            /* Base font styles */
+            html,body {
+              font-family: 'Muli', sans-serif;
+            }
+          `}</style>
+        </Wrapper>
       </Container>
     )
   }
