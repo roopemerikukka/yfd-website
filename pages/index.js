844a8d55ad2a4becb2e27147b8aed52e3ba120c6
Daniel Patty <drx.patty@gmail.com>
2018-07-20 21:10:47 +0300

    [ref #45] Adds a featured product card
    - Adds a card for presenting featured products.
    - Temporary adjustments to index.js for styling.
    
    This closes #45.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { textStyles } from '../common/textStyles'
 import LinkButton from '../components/linkButton'
 import FeaturedPullUp from '../components/featuredPullUp'
+import PageHeading from '../components/pageHeading'
 
 export default class extends React.Component {
   static async getInitialProps({ contentfulClient }) {
@@ -16,8 +17,9 @@ export default class extends React.Component {
   render() {
     return (
       <React.Fragment>
+        {this.props.products.total > 1 ? <PageHeading heading='Our products'/> : '' } 
         <FeaturedPullUp products={this.props.products.items} />
-        <h1 className={textStyles.h1.className}>Young Finnish Design</h1>
+        <PageHeading heading='About the company'/>
         <p className={textStyles.copy.className}>
           This is a test paragraph used to test base text styles. Here's a link to the <a href="https://stage.youngfinnishdesign.com/">staging environment</a> of the YFD website. This word <b>right here</b> is bolded. So is <strong>this</strong> but with a different tag. The total number of entries in Contentful is currently <b>{ this.props.products.total }</b>. Isn't that great?
         </p>
