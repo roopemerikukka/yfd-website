b16f57eb37b2e287cc811a9122f91d5407113499
Daniel Patty <drx.patty@gmail.com>
2018-07-25 21:42:10 +0300

    [ref #66] Fixes card styling (#67)
    - Moves the Link component to wrap at differently
    - Still some issues with the media queries not showing correctly
    
    This closes #66.

--- common/breakpoints.js
+++ common/breakpoints.js
@@ -1,9 +1,9 @@
 const breakpoints = {
-  small: "35em" /* 560px / 16 */,
-  medium: "48em" /* 768px / 16 */,
-  large: "64em" /* 1024px / 16 */,
-  xlarge: "80em" /* 1280px / 16 */,
-  xxlarge: "120em" /* 1920px / 16 */
+  small: '35em',
+  medium: '48em',
+  large: '64em',
+  xlarge: '80em',
+  xxlarge: '120em'
 };
 
 export default breakpoints;
--- components/featuredProductCard.js
+++ components/featuredProductCard.js
@@ -4,109 +4,106 @@ import { textStyles } from '../common/textStyles'
 import { BLACK, BLUE } from '../common/colors'
 
 export default ({ product }) => (
-  <div className='featured-product-card'>
-    <Link route={`/products/${product.fields.slug}`}>
-      <a>
-        <div className='featured-product-card__title' data-slug={product.fields.slug}>
-          <h2 className={textStyles.cardTitle.className}>{product.fields.name}</h2>
-          <h3 className={textStyles.cardPrice.className}>{product.fields.price}€</h3>
-        </div>
-        <img  className='featured-product-card__img' 
-              srcSet={`
-              ${product.fields.images[0].fields.file.url}?w=600&h=600 600w,
-              ${product.fields.images[0].fields.file.url}?w=1000&h=688 1000w,
-              ${product.fields.images[0].fields.file.url}?w=1280&h=880 1280w,
-              ${product.fields.images[0].fields.file.url}?w=1600&h=1100 1600w,
-              ${product.fields.images[0].fields.file.url} 2000w,
-              `}
-              sizes=" 
-              (max-width: 320px) 280px,
-              (max-width: 768px) 768px,
-              (max-width: 1024px) 1024px,
-              (max-width: 1280px) 1280px,
-              2000px"
-              src={`${product.fields.images[0].fields.file.url}?w=1000&h=1000`} alt={product.fields.images[0].fields.title}
-              />
-        </a>
-    </Link>
-    <style jsx>{`
-      .featured-product-card {
-        display: block;
-        width: 100%;
-        cursor: pointer;
-      }
-
-      .featured-product-card__title {
-        position: relative;
-        display: flex;
-        width: 100%;
-        justify-content: space-between;
-        align-items: flex-end;
-        margin-bottom: 12px;
-      }
-      
-      .featured-product-card__title h2 {
-        width: 70%;
-        margin: 0;
-        color: ${BLACK};
-        transition: color 0.12s ease-in;
-      }
-
-      .featured-product-card__title h3 {
-        width: 30%;
-        margin: 0;
-        text-align: right;
-        color: ${BLACK};
-        transition: color 0.12s ease-in;
-      }
-
-      .featured-product-card:hover {
-        text-decoration: none;
-      }
-
-      .featured-product-card:hover h2,
-      .featured-product-card:hover h3 {
-        color: ${BLUE};
-        text-decoration: none;
-      }
-
-      .featured-product-card__img {
-        max-width: 100%;
-      }
-
-      @media screen and (min-width: ${breakpoints.xlarge}) {
+  <Link route={`/products/${product.fields.slug}`}>
+    <a className='featured-product-card'>
+      <div className='featured-product-card__title'>
+        <h2 className={textStyles.cardTitle.className}>{product.fields.name}</h2>
+        <h3 className={textStyles.cardPrice.className}>{product.fields.price}€</h3>
+      </div>
+      <img  className='featured-product-card__img' 
+            srcSet={`
+            ${product.fields.images[0].fields.file.url}?w=600&h=600 600w,
+            ${product.fields.images[0].fields.file.url}?w=1000&h=688 1000w,
+            ${product.fields.images[0].fields.file.url}?w=1280&h=880 1280w,
+            ${product.fields.images[0].fields.file.url}?w=1600&h=1100 1600w,
+            ${product.fields.images[0].fields.file.url} 2000w,
+            `}
+            sizes=" 
+            (max-width: 320px) 280px,
+            (max-width: 768px) 768px,
+            (max-width: 1024px) 1024px,
+            (max-width: 1280px) 1280px,
+            2000px"
+            src={`${product.fields.images[0].fields.file.url}?w=1000&h=1000`} alt={product.fields.images[0].fields.title}
+            />
+      <style jsx>{`
         .featured-product-card {
+          display: block;
+          width: 100%;
+          cursor: pointer;
+        }
+        
+        .featured-product-card__title {
+          position: relative;
           display: flex;
-          flex-direction: column;
+          width: 100%;
+          justify-content: space-between;
+          align-items: flex-end;
+          margin-bottom: 12px;
         }
-
-        .featured-product-card__title {
-          flex-direction: column;
-          margin-top: 24px;
-          order: 2;
-        }
-
+        
         .featured-product-card__title h2 {
-          width: 100%;
-          margin-bottom: 8px;
+          width: 70%;
+          margin: 0;
+          color: ${BLACK};
+          transition: color 0.12s ease-in;
         }
-
+        
         .featured-product-card__title h3 {
-          width: 100%;
-          text-align: left;
+          width: 30%;
+          margin: 0;
+          text-align: right;
+          color: ${BLACK};
+          transition: color 0.12s ease-in;
         }
-
+        
+        .featured-product-card:hover {
+          text-decoration: none;
+        }
+        
+        .featured-product-card:hover h2,
+        .featured-product-card:hover h3 {
+          color: ${BLUE};
+          text-decoration: none;
+        }
+        
         .featured-product-card__img {
-          order: 1;
+          max-width: 100%;
         }
-      }
-
-      @media screen and (min-width: ${breakpoints.xlarge}) {
-        .featured-product-card__title {
-          margin-top: 32px;
+        
+        @media screen and (min-width: ${breakpoints.large}) {
+          .featured-product-card {
+            display: flex;
+            flex-direction: column;
+          }
+          
+          .featured-product-card__title {
+            flex-direction: column;
+            margin-top: 24px;
+            order: 2;
+          }
+          
+          .featured-product-card__title h2 {
+            width: 100%;
+            margin-bottom: 8px;
+          }
+          
+          .featured-product-card__title h3 {
+            width: 100%;
+            text-align: left;
+          }
+          
+          .featured-product-card__img {
+            order: 1;
+          }
         }
-      }
-
-    `}</style>
-  </div>
+        
+        @media screen and (min-width: ${breakpoints.xlarge}) {
+          .featured-product-card__title {
+            margin-top: 32px;
+          }
+        }
+      `}</style>
+    </a>
+  </Link>
 )
