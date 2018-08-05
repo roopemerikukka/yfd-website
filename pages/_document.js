a5859685b95426b9b0b8463bff587113a23eccf9
Joonas Salovaara <joonas.salovaara@gmail.com>
2018-08-05 21:30:08 +0200

    [ref #100] Add favicon (#106)
    - Adds a bunch of favicons for best browser support
    
    This closes #100.

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,15 @@ export default class MyDocument extends Document {
           />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
           <link href="https://fonts.googleapis.com/css?family=Muli:400,600,600i,700" rel="stylesheet" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+          <link rel="manifest" href="/static/site.webmanifest" />
+          <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#000000" />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="msapplication-config" content="/static/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
